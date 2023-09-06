9aae8c2c0f8392dd020946a8bedcfef1b9b3f7f4
Dinoplant <143848581+Dinoplant@users.noreply.github.com>
2023-09-06 14:13:25 -0700

    testing changes

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 const theButton = document.getElementById('button');
 const panels = document.querySelectorAll('.panel');
 const closeButton = document.getElementById('close');
-
+const FollowButton = document.getElementById('Flw');
+const BathroomCounter = document.getElementById("BrCounter")
 
 // Add the event listener to the profile button
 theButton.addEventListener('click', () => {
@@ -18,3 +19,18 @@ closeButton.addEventListener('click', () => {
     panel.classList.remove('open');
   });
 });
+
+// Add the event listener to the follow button
+FollowButton.addEventListener('click', () => {
+  window.open("https://github.com/ILCart/cart-profile").focus()
+});
+
+// Add the event listener to the follow button
+FollowButton.addEventListener('click', () => {
+  window.open("https://github.com/ILCart/cart-profile").focus()
+});
+// This just keeps counting up for the "? days without using the bathroom"
+const Today = new Date() //Gets todays date
+const Start = new Date('August 25, 2023 0:0:0') //Gets the date on August 25th
+const Diff = Today - Start // Gets the difference between the two
+BathroomCounter.textContent = parseInt(Diff/(1000*60*60*24)) //Parses it into milliseconds to days ms>s>h>d
